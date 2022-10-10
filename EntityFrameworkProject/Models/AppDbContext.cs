2db31cd8a84cf383d62a8e7562af5f5114be971f
Daniel ikechukwu <danielikechukwu495@gmail.com>
2022-10-10 13:41:04 +0100

    Add project files.

---
+++ EntityFrameworkProject/Models/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+
+namespace EntityFrameworkProject.Models
+{
+    public class AppDbContext : DbContext
+    {
+       //creating a constructor class
+       public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+
+
+    }
+}
---
+++ EntityFrameworkProject/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EntityFrameworkProject.Models
+{
+    public class Employee
+    {
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set;}
+
+    }
+}
